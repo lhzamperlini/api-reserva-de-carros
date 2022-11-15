b92a7b670ee9817b0ebbf354c7f0b4d92b0dca00
lhzamperlini <lhzamperlini@gmail.com>
2022-11-15 11:48:53 -0300

    chore: salvar usuario no banco de dados com senha criptografada

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,4 +1,6 @@
 const Usuario = require('../models/Usuario')
+const bcrypt = require('bcrypt')
+const { username } = require('../config/database')
 
 module.exports = class UsuarioController {
     static async registrar(req, res){
@@ -24,12 +26,32 @@ module.exports = class UsuarioController {
             }
         
         //Checar se usuario existe
-            usuarioExistente = await Usuario.findOne( { login:login } )
+            const usuarioExistente = await Usuario.findOne( { login:login } )
             if(usuarioExistente){
                 res.status(422).json({
                      message: 'O usuario já existe, por favor use outro login.' 
                 })
                 return
             }
+        //Criptografar Senha
+        const salt = await bcrypt.genSalt(12)
+        const senhaCriptografada = await bcrypt.hash(senha, salt)
+
+        //Criar objeto de usuario
+        const usuario = {
+            login,
+            senha: senhaCriptografada
         }
-}
+        try{
+            await Usuario.create(usuario)
+            .then((usuario)=>{
+                res.status(201).json({
+                    message: 'Usuario Criado com sucesso.',
+                    usuario
+                })
+            })
+        }
+        catch(error){
+            res.status(500).json({message: error})
+        }  
+    }}
