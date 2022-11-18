9bbbf9b7656c781d854808fb89821f9a3b7861b4
lhzamperlini <lhzamperlini@gmail.com>
2022-11-18 07:10:06 -0300

    chore: adicionar a função de verificar token e pegar usuario pelo token

---
+++ src/helpers/pegar-usuario-pelo-token.js
@@ -0,0 +1,17 @@
+const jwt = require ('jsonwebtoken')
+const Usuario = require('../models/Usuario')
+
+
+const pegarUsuarioPorToken = async (token) => {
+    if(!token){
+        return res.status(401).json({
+            message: 'Acesso Negado!'
+        })
+    }
+
+    const decodificado = jwt.verify(token, 'nossoSegredo')
+    const usuarioId = decodificado.id
+
+    const usuario =  await Usuario.findOne({where:{id: usuarioId}})
+    return usuario
+}
